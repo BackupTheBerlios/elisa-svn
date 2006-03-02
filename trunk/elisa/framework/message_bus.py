d65f0af10dc88ef22ffba8bee39e07ca399f6bca
pnormand <pnormand@decd430a-060a-0410-a02b-ee8e357ddcca>
2006-03-02 20:47:51 +0000

    callbacks are now mapped by instance instead of by class

--- trunk/elisa/framework/message_bus.py
+++ trunk/elisa/framework/message_bus.py
@@ -1,5 +1,6 @@
 from elisa.framework.message import Message
 import os, sys
+import copy
 
 def calling_frame():
     """ Calling sys frame """
@@ -32,7 +33,7 @@ class _MessageBus:
 
     # mapping of class -> callback
     callbacks = {}
-    
+
     def send_message(self, message, receiver=None):
         assert isinstance(message, Message)
         frame = calling_frame()
@@ -40,17 +41,16 @@ class _MessageBus:
         self.queue.insert(0, (message, sender, receiver))
 
     def register(self, instance, callback):
-        #
-        klass = callback.im_class
-        self.callbacks[klass] = (callback, instance)
-
+        self.callbacks[instance] = callback
+        
     def dispatch_messages(self):
+
         while self.queue:
             message, sender, receiver = self.queue.pop()
             if receiver:
                 # dispatch to receiver
-                callback, instance = self.callbacks.get(receiver.__class__,
-                                                        lambda x,y: None)
+                callback = self.callbacks.get(receiver,
+                                              lambda x,y: None)
                 result = callback(receiver, message, sender)
                 # flush the queue and exit if callback returned False
                 if result == False:
@@ -58,11 +58,10 @@ class _MessageBus:
                     break
             else:
                 # broadcast to all registered entities
-                for receiver, callback in self.callbacks.iteritems():
-                    callback, instance = self.callbacks.get(receiver, lambda x,y: None)
-                    #import pdb; pdb.set_trace()
-                    #print callback
-                    result = callback(instance, message, sender)
+                callbacks = copy.copy(self.callbacks)
+                for receiver, callback in callbacks.iteritems():
+                    result = callback(receiver, message, sender)
+                    
                     # flush the queue and exit if callback returned False
                     if result == False:
                         self.queue = []
