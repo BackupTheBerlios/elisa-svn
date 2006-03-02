7c2d2f1c4f98669e649bad16978a2d02a815d994
pnormand <pnormand@decd430a-060a-0410-a02b-ee8e357ddcca>
2006-03-02 15:49:28 +0000

    handle a messages queue and a callback mapping

--- trunk/elisa/framework/message_bus.py
+++ trunk/elisa/framework/message_bus.py
@@ -2,11 +2,24 @@ from elisa.framework.message import Message
 
 class MessageBus:
 
+    # list of (message, sender, receiver) tuples
+    queue = []
+
+    # mapping of class -> callback
+    callbacks = {}
+    
     def send_message(self, message, sender, receiver):
         assert isinstance(message, Message)
-        
-        if hasattr(receiver, 'on_message'):
-            receiver.on_message(message, sender)
+        self.queue.insert(0, (message, sender, receiver))
+
+    def register(self, obj, callback):
+        self.callbacks[obj] = callback
+
+    def dispatch_messages(self):
+        while self.queue:
+            message, sender, receiver = self.queue.pop()
+            callback = self.callbacks.get(receiver.__class__, lambda x,y: None)
+            callback(receiver, message, sender)
 
 if __name__ == '__main__':
     
@@ -18,5 +31,13 @@ if __name__ == '__main__':
         def on_message(self, message, sender):
             print 'Got message %r from %r' % (message, sender)
             print message.data
-            
-    bus.send_message(Message('sentence','Hello you'), Foo(), Bar())
+
+    f = Foo()
+    b = Bar()
+    bus.register(Bar, Bar.on_message)
+
+    for i in range(5):
+        data = '%s. Hello you' % i
+        bus.send_message(Message('sentence', data), f, b)
+
+    bus.dispatch_messages()
