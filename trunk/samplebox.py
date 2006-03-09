a2b05b711b6992050e79a2a90671862e7016f58a
pnormand <pnormand@decd430a-060a-0410-a02b-ee8e357ddcca>
2006-03-09 16:21:46 +0000

    samplebox can now be called w/ custom config file as command line argument

--- trunk/samplebox.py
+++ trunk/samplebox.py
@@ -1,5 +1,6 @@
 #sample bnx using framework
 import os
+import sys
 
 # set this to 0 to enable PyChecker (time-consuming!)
 os.environ['PYCHECKER_DISABLED'] = '1'
@@ -11,14 +12,20 @@ except ImportError:
     pass
 
 
-from elisa.framework import application, log
-import time
+from elisa.framework import application
 
 class BoxApplication(application.Application): pass
 
 if __name__ == '__main__':
-    main = BoxApplication()
 
+    args = sys.argv[1:]
+
+    if len(args):
+        config_file = args[0]
+        main = BoxApplication(config_file)
+    else:
+        main = BoxApplication()
+        
     main.run()
 
     
